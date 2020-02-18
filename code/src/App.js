60352ca22907ab7534fe63af46f33a677c2e5c4a
horsefacegirl <51134090+horsefacegirl@users.noreply.github.com>
2020-02-18 12:45:38 +0100

    Update url
    changed database

--- code/src/App.js
+++ code/src/App.js
@@ -1,50 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import HappyThought from "./Components/HappyThought"
-import HappyForm from "./Components/HappyForm"
-import { AppWrapper } from 'Components/styles'
+import React, { useEffect, useState } from "react";
+import HappyThought from "./Components/HappyThought";
+import HappyForm from "./Components/HappyForm";
+import { AppWrapper } from "Components/styles";
 
 export const App = () => {
-
-  const urlThoughts = "https://technigo-thoughts.herokuapp.com/"
-  const [thoughts, setThoughts] = useState([])
+  const urlThoughts = "https://horsefacegirl-happy-thoughts.herokuapp.com/thoughts";
+  const [thoughts, setThoughts] = useState([]);
   const getThoughts = () => {
     fetch(urlThoughts)
       .then(res => res.json())
-      .then(json => setThoughts(json))
-  }
+      .then(json => setThoughts(json));
+  };
 
   useEffect(() => {
-    getThoughts()
-  }, [])
+    getThoughts();
+  }, []);
 
-  const handleFormSubmit = (message) => {
+  const handleFormSubmit = message => {
     fetch(urlThoughts, {
       method: "POST",
       body: JSON.stringify({ message }),
       headers: { "Content-Type": "application/JSON" }
-    })
-      .then(() => {
-        getThoughts()
-      })
-  }
+    }).then(() => {
+      getThoughts();
+    });
+  };
 
-  const handleAddHearts = (id) => {
-    fetch(`https://technigo-thoughts.herokuapp.com/${id}/like`, {
+  const handleAddHearts = id => {
+    fetch(`https://horsefacegirl-happy-thoughts.herokuapp.com/thoughts/${id}/like`, {
       method: "POST",
       headers: { "Content-Type": "application/JSON" }
-    })
-      .then(() => {
-        getThoughts()
-      })
-  }
+    }).then(() => {
+      getThoughts();
+    });
+  };
 
   return (
     <AppWrapper>
-      <HappyForm onFormSubmit={(message) => handleFormSubmit(message)} />
+      <HappyForm onFormSubmit={message => handleFormSubmit(message)} />
       {thoughts.map(thought => (
-        <HappyThought key={thought._id} thought={thought} addHearts={() => handleAddHearts(thought._id)} />
-
+        <HappyThought
+          key={thought._id}
+          thought={thought}
+          addHearts={() => handleAddHearts(thought._id)}
+        />
       ))}
     </AppWrapper>
-  )
-}
+  );
+};
